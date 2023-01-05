9a2fd613d93a4828452c8a1522bd2306bf9c076c
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-05 21:08:53 +0100

    feat: add head

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Enzo Mourany</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
