bb79f360ccdd7d713d0dcda681e196c8daee97c4
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-06 11:09:13 +0100

    feat: add PostPreview

---
+++ components/PostPreview.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+import { PostMetadata } from "./PostMetadata";
+
+const postPreview = (props: PostMetadata) => {
+    return (
+        <div key={props.slug}>
+        <Link href={`/posts/${props.slug}`}>
+        <h2>{props.title}</h2>
+        </Link>
+        </div>
+        )
+    };
+    
+    export default postPreview;
