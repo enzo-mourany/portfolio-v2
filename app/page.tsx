fedce360b0bf8949c0611158a77c372fd22ca338
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-06 10:44:18 +0100

    fix: slug -> matterResult

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,43 @@
-const HomePage = () => {
-    return <h1>
-        Hello World
-    </h1>
+import React from 'react';
+import { type NextPage } from "next";
+import Link from 'next/link';
+import fs from 'fs';
+import matter from 'gray-matter';
+
+import type { PostMetadata } from '../components/PostMetadata';
+
+const getPostMetadata = (): PostMetadata[] => {
+  const folder = 'posts/';
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith('.md'));
+  
+  // Get gray-matter data from each post
+  const posts = markdownPosts.map((fileName) => {
+    const fileContent = fs.readFileSync(`${folder}${fileName}`, 'utf8');
+    const matterResult = matter(fileContent);
+    return {
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      subtitle: matterResult.data.subtitle,
+      slug: fileName.replace('.md', ''),
+    };
+  });
+
+  return posts;
+};
+
+const HomePage: NextPage = () => {
+  const postMetadata = getPostMetadata();
+  const postPreview = postMetadata.map((post) => (
+    <div key={post.slug}>
+      <Link href={`/posts/${post.slug}`}>
+        <h2>{post.title}</h2>
+      </Link>
+    </div>
+  ));
+  return (
+    <div>{postPreview}</div>
+  )
 };
 
 export default HomePage;
