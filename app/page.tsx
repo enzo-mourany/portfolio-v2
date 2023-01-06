a6a250809ebaa9f073fa743ee7de120a97f3d23d
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-06 11:09:34 +0100

    feat: use PostPreview

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,10 @@
-import React from 'react';
-import { type NextPage } from "next";
-import Link from 'next/link';
-import fs from 'fs';
-import matter from 'gray-matter';
+import getPostMetadata from '../components/getPostMetadata';
+import PostPreview from '../components/PostPreview';
 
-import type { PostMetadata } from '../components/PostMetadata';
-
-const getPostMetadata = (): PostMetadata[] => {
-  const folder = 'posts/';
-  const files = fs.readdirSync(folder);
-  const markdownPosts = files.filter((file) => file.endsWith('.md'));
-  
-  // Get gray-matter data from each post
-  const posts = markdownPosts.map((fileName) => {
-    const fileContent = fs.readFileSync(`${folder}${fileName}`, 'utf8');
-    const matterResult = matter(fileContent);
-    return {
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-      subtitle: matterResult.data.subtitle,
-      slug: fileName.replace('.md', ''),
-    };
-  });
-
-  return posts;
-};
-
-const HomePage: NextPage = () => {
+const HomePage = () => {
   const postMetadata = getPostMetadata();
   const postPreview = postMetadata.map((post) => (
-    <div key={post.slug}>
-      <Link href={`/posts/${post.slug}`}>
-        <h2>{post.title}</h2>
-      </Link>
-    </div>
+    <PostPreview key={post.slug} {...post} />
   ));
   return (
     <div>{postPreview}</div>
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -2,12 +2,21 @@ import fs from 'fs';
 import Markdown from 'markdown-to-jsx';
 import matter from 'gray-matter';
 
+import getPostMetadata from '../../../components/getPostMetadata';
+
 const getPostContent = (slug: string) => {
   const folder = 'posts/';
   const file = `${folder}${slug}.md`;
   const content = fs.readFileSync(file, 'utf8');
   const matterResult = matter(content);
   return matterResult;
+};
+
+export const generateStaticParams = async () => {
+  const posts = getPostMetadata();
+  return posts.map(post => ({
+    slug: post.slug,
+  }));
 };
 
 const PostPage = (props: any) => {
