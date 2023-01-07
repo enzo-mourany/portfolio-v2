9b35031882cd98d1a7c501a4cf71b40da5344d3a
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-07 10:47:13 +0100

    feat: add HomePage

---
+++ app/HomePage.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/HomePage.module.scss';
+
+const HomePage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.title}>
+          <h1>Frontend web and web mobile developer based in France</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
