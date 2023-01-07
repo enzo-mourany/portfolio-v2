f30e2a3886eaaf06b94fb6ae5c4af526851fa4ca
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-07 10:48:26 +0100

    fix: use client page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,38 @@
+"use client";
+
+import Link from 'next/link';
+
+import '../styles/globals.css';
+import HomePage from './HomePage';
+import Hamburger from '../components/input/Hamburger';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+    const header = (
+        <header>
+          <div className="flex flex-row justify-between text-center bg-black p-8">
+            <div className="w-48">
+                <Link href="/">
+              <p className="text-white text-xl font-normal">Enzo Mourany</p>
+            </Link>
+            </div>
+            <div className="w-48">
+                <p className="text-white text-sm font-extralight uppercase">Portfolio ©2023</p>
+            </div>
+            <div className="w-48">
+                <Hamburger />
+            </div>
+          </div>
+        </header>
+      );
   return (
     <html>
         <head />
         <body>
-            <header>Header</header>
-            {children}
+            <div>
+                {header}
+                <HomePage />
+                {children}
+            </div>
         </body>
     </html>
   )
