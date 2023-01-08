2e923e73986355732180cabb47cb68f70e3ad087
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-08 22:17:10 +0100

    fix: header position fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import Hamburger from '../components/input/Hamburger';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     const header = (
         <header>
-          <div className="flex flex-row justify-between text-center bg-black p-8">
+          <div className="flex flex-row justify-between fixed w-full text-center p-8">
             <div className="w-48">
                 <Link href="/">
               <p className="text-white text-xl font-normal">Enzo Mourany</p>
@@ -27,7 +27,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html>
         <head />
-        <body>
+        <body className="bg-white dark:bg-black">
             <div>
                 {header}
                 <HomePage />
