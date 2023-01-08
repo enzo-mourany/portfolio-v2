213f6be9dc97866f61d450d74fb779f741f54dc3
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-09 00:02:09 +0100

    feat: add SelectedProjects component

---
+++ components/SelectedProjects.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ProjectBox from './ProjectBox';
+
+const SelectedProjects: React.FC = () => {
+    return (
+        <div>
+            <ProjectBox />
+        </div>
+    )
+};
+
+export default SelectedProjects;
