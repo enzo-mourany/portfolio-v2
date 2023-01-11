43676bdc541f6f8b3256c811d8c45fc715cdcc31
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-11 17:00:52 +0100

    feat: add framer-motion

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,30 @@
 'use client';
 
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 import '../styles/globals.css';
 import Hamburger from '../components/input/Hamburger';
+import menu from './menu';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const variants = {
+    open: { display: 'flex', opacity: 1 },
+    closed: { display: 'hidden', opacity: 0 },
+  };
+
   const header = (
     <header>
       <div className='flex flex-row justify-between fixed w-full text-center p-8'>
         <div className='w-48'>
-          <Link href='/'>
+          <Link href='/' onClick={() => toggleMenu()}>
             <p className='text-white text-xl font-normal'>Enzo Mourany</p>
           </Link>
         </div>
@@ -23,12 +37,25 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </div>
     </header>
   );
+
+  const menu = (
+    <motion.div
+      className='hidden flex-col justify-center items-center fixed w-full h-full bg-black opacity-50'
+      variants={variants}
+      initial='closed'
+      animate={isMenuOpen ? 'open' : 'closed'}
+    >
+      <p>Hello</p>
+    </motion.div>
+  )
+
   return (
     <html>
       <head />
       <body className='bg-white dark:bg-black'>
         <div>
           {header}
+          {menu}
           {children}
         </div>
       </body>
