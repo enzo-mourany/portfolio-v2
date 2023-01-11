a2141a4158ba1f4ec15177bfb707a6920f1916bb
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-11 15:53:25 +0100

    fix: eslint quotes error

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
     fontFamily: {
-      "manrope": ["Manrope", "sans-serif"],
+      manrope: ['Manrope', 'sans-serif'],
     },
     screens: {
-      xs: "480px",
-      sm: "640px",
-      md: "767px",
-      lg: "1024px",
-      xl: "1280px",
+      xs: '480px',
+      sm: '640px',
+      md: '767px',
+      lg: '1024px',
+      xl: '1280px',
     },
   },
   plugins: [],
