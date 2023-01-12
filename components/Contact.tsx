2473748bce1e85157456749449aabc2a6d356cab
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-12 22:17:06 +0100

    fix: textHoverAnimation bug

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,22 +1,12 @@
 'use client';
 
-import { motion } from 'framer-motion';
-import React, { useState } from 'react';
+import React from 'react';
 
 import styles from '../styles/Contact.module.scss';
-import LineButton from './input/LineButton';
+
+import TextHoverAnimation from './effect/TextHoverAnimation';
 
 const Contact: React.FC = () => {
-  const [hover, setHover] = useState(false);
-  const visibleTextVariants = {
-    hover: { y: -50, opacity: 0, transition: { duration: 0.2 } },
-    normal: { y: 0, opacity: 1, transition: { duration: 0.2 } }
-  };
-
-  const hiddenTextVariants = {
-    hover: { y: -135, opacity: 1, transition: { duration: 0.2 } },
-    normal: { y: -50, opacity: 0, transition: { duration: 0.2 } }
-  };
 
   return (
     <div className={styles.contact}>
@@ -36,30 +26,8 @@ const Contact: React.FC = () => {
         <a href='mailto:enzo.mourany@outlook.com'>enzo.mourany@outlook.com</a>
       </div>
       <div className={styles.links}>
-        <a
-          href='https://github.com/enzo-mourany'
-          target='_blank'
-          rel='noreferrer'
-          onMouseEnter={() => setHover(true)}
-          onMouseLeave={() => setHover(false)}
-          className={styles.links__link}
-        >
-          <motion.span
-            className={styles.links__link__text}
-            variants={visibleTextVariants}
-            animate={hover ? 'hover' : 'normal'}
-          >
-            GitHub
-          </motion.span>
-          <motion.span
-            className={styles.links__link__text__hover}
-            variants={hiddenTextVariants}
-            animate={hover ? 'hover' : 'normal'}
-          >
-            GitHub
-          </motion.span>
-        </a>
-        <a href='https://www.linkedin.com/in/enzo-mourany/' target='_blank' rel='noreferrer'>LinkedIn</a>
+        <TextHoverAnimation text='GitHub' url='https://github.com/enzo-mourany' />
+        <TextHoverAnimation text='LinkedIn' url='https://www.linkedin.com/in/enzo-mourany/' />
       </div>
     </div>
   )
--- components/effect/TextHoverAnimation.tsx
+++ components/effect/TextHoverAnimation.tsx
@@ -11,80 +11,41 @@ interface TextHoverAnimationProps {
 }
 
 const TextHoverAnimation: React.FC<TextHoverAnimationProps> = ({ text, url }) => {
-  const lettersText: string[] = text.split('');
-
   const [hover, setHover] = useState(false);
-
-  const container = {
-    hidden: { opacity: 1 },
-    visible: (i = 1) => ({
-      opacity: 1,
-      transition: { staggerChildren: 0.12, delayChildren: 0.5 + i * 0.04 },
-    })
+  const visibleTextVariants = {
+    hover: { y: -50, opacity: 0, transition: { duration: 0.2 } },
+    normal: { y: 0, opacity: 1, transition: { duration: 0.2 } }
   };
 
-  const visibleChild = {
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        type: 'spring',
-        damping: 12,
-        stiffness: 100,
-      },
-    },
-    hidden: {
-      opacity: 0,
-      y: 20,
-      transition: {
-        type: 'spring',
-        damping: 12,
-        stiffness: 100,
-      },
-    },
-  };
-
-  const hiddenChild = {
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        type: 'spring',
-        damping: 12,
-        stiffness: 100,
-      },
-    },
-    hidden: {
-      opacity: 0,
-      y: 20,
-      transition: {
-        type: 'spring',
-        damping: 12,
-        stiffness: 100,
-      },
-    },
+  const hiddenTextVariants = {
+    hover: { y: -135, opacity: 1, transition: { duration: 0.2 } },
+    normal: { y: -50, opacity: 0, transition: { duration: 0.2 } }
   };
 
   return (
     <motion.a
+      href={url}
+      target='_blank'
+      rel='noopener noreferrer'
       className={styles.text__container}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
-      href={url}
-      variants={container}
       initial='hidden'
-      animate={hover ? 'visible' : 'hidden'}
-      style={{ display: 'flex', flexWrap: 'wrap' }}
     >
-      {lettersText.map((letter, index) => (
-        <motion.span
-          className={styles.text__letter}
-          variants={visibleChild}
-          style={{ marginRight: '5px', wordWrap: 'normal', whiteSpace: 'pre-wrap'}}
-          key={index}>
-          {letter}
-        </motion.span>
-      ))}
+      <motion.span
+        className={styles.text__letter}
+        variants={visibleTextVariants}
+        animate={hover ? 'hover' : 'normal'}
+      >
+        {text}
+      </motion.span>
+      <motion.span
+        className={styles.text__letter__hidden}
+        variants={hiddenTextVariants}
+        animate={hover ? 'hover' : 'normal'}
+      >
+        {text}
+      </motion.span>
     </motion.a>
   );
 };
