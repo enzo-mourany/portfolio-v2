cf0dddae07e93b41399d4d981f4720a8b2dbc214
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-12 13:45:44 +0100

    feat: responsive navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,16 +22,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   const header = (
     <header>
-      <div className='flex flex-row justify-between fixed z-10 w-full text-center p-8'>
-        <div className='w-48'>
+      <div className='flex flex-row justify-between items-center fixed z-10 w-full text-center p-6 xs:p-8'>
+        <div className='w-1/2 xs:w-48 text-left sm:text-center'>
           <Link href='/'>
-            <p className='text-white text-xl font-normal'>Enzo Mourany</p>
+            <p className='text-white text-sm sm:text-xl font-normal'>Enzo Mourany</p>
           </Link>
         </div>
-        <div className='w-48'>
+        <div className='hidden sm:block w-48'>
           <p className='text-white text-sm font-extralight uppercase'>Portfolio ©2023</p>
         </div>
-        <div className='flex justify-center w-48' onClick={() => toggleMenu()}>
+        <div className='flex justify-end sm:justify-center w-1/2 xs:w-48' onClick={() => toggleMenu()}>
           <Hamburger isMenuOpen={isMenuOpen} />
         </div>
       </div>
