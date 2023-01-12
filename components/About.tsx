833d1e0ba46232c6671537b1926939e882dc52bb
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-12 10:02:28 +0100

    feat: add Link Linebutton

--- components/About.tsx
+++ components/About.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+
 import styles from '../styles/About.module.scss';
 import LineButton from './input/LineButton';
 
@@ -6,7 +11,9 @@ const About = () => {
     <div className={styles.wrapper}>
       <p className={styles.title}>MY PHYLOSOPHY</p>
       <p className={styles.description}>I create inclusive, accessible digital products, with experience working with startups and enterprise products. I believe in using product design as a tool to elevate human interaction with technology that scale.</p>
-      <LineButton text='About Me' />
+      <Link href='/about'>
+        <LineButton text='About Me' />
+      </Link>
     </div>
   );
 };
