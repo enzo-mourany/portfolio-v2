9598c0dedb747301ecaf9a956a56fd917a898997
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-13 12:24:54 +0100

    feat: add onMouseDown animation

--- components/input/LineButton.tsx
+++ components/input/LineButton.tsx
@@ -11,6 +11,7 @@ interface LineButtonProps {
 
 const LineButton: React.FC<LineButtonProps> = ({text}) => {
   const [hover, setHover] = useState(false);
+  const [clicked, setClicked] = useState(false);
   const visibleTextVariants = {
     hover: { y: 30, opacity: 0, transition: { duration: 0.2 } },
     normal: { y: 0, opacity: 1, transition: { duration: 0.2 } }
@@ -24,8 +25,11 @@ const LineButton: React.FC<LineButtonProps> = ({text}) => {
   return (
     <div
       className={styles.button}
+      style={{ backgroundColor: clicked ? '#fff' : 'transparent' }}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
+      onMouseDown={() => setClicked(true)}
+      onMouseUp={() => setClicked(false)}
     >
       <motion.p
         className={styles.button_text}
@@ -36,6 +40,7 @@ const LineButton: React.FC<LineButtonProps> = ({text}) => {
       </motion.p>
       <motion.p
         className={styles.button_text_hover}
+        style={{ color: clicked ? '#000' : '#fff' }}
         variants={hiddenTextVariants}
         animate={hover ? 'hover' : 'normal'}
       >
