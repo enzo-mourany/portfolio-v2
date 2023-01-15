6f262787f755b05d9f23cb84a285e46fd9039bbc
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-15 13:56:22 +0100

    fix: locomotive scroll dependencies bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
-import LocomotiveScroll from 'locomotive-scroll';
 
 import '../styles/globals.css';
 import Hamburger from '../components/input/Hamburger';
