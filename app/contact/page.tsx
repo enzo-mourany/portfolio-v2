fa63eda1e2de9f625ff0164a0354681f01b0d15c
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-16 10:54:38 +0100

    feat: add style

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import styles from '../../styles/ContactPage.module.scss';
+
 import Contact from '../../components/Contact';
 
 const Page: React.FC = () => {
   return (
-    <div>
+    <div className={styles.contact__container}>
       <Contact />
     </div>
   );
