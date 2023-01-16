30e153679fe7848541378e19be65dfbf669fd2d2
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-16 10:51:44 +0100

    feat: add contact component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Contact from '../../components/Contact';
+
 const Page: React.FC = () => {
   return (
     <div>
-      <p>Contact</p>
+      <Contact />
     </div>
   );
 };
