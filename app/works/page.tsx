f758d39142908d251fa18a2cc5481f2831ca2158
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-16 11:05:00 +0100

    fix: nextPage

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,11 +1,10 @@
-import { NextPage } from 'next';
 import React from 'react';
 
 import styles from '../../styles/HomePage.module.scss';
 import getPostMetadata from '../../components/getPostMetadata';
 import PostPreview from '../../components/PostPreview';
 
-const Page: NextPage = () => {
+const Page = () => {
   const postMetadata = getPostMetadata();
   const postPreview = postMetadata.map((post) => (
     <PostPreview key={post.slug} {...post} />
