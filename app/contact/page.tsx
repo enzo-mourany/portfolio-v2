6472a465b75f81f93a770dec874be11bc9ead1e0
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-18 21:33:20 +0100

    feat: style

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import styles from '../../styles/ContactPage.module.scss';
 
 import Contact from '../../components/Contact';
+import ScrollingText from '../../components/ScrollingText';
 
 const Page: React.FC = () => {
   return (
     <div className={styles.contact__container}>
-      <Contact />
+      <div className={styles.contact__wrapper}>
+        <ScrollingText text='contact' />
+        <Contact />
+      </div>
     </div>
   );
 };
