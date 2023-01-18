fc53c315e83a716fa61dead770d2b2edb46469d1
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-18 21:10:43 +0100

    fix: responsive

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
 
 import styles from '../../styles/AboutPage.module.scss';
 import ScrollingText from '../../components/ScrollingText';
