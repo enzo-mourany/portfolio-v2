305c6e9cc920df1fb789f56fd1f64e2b2e6959b8
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-28 11:18:23 +0100

    feat: add Links

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -14,11 +14,44 @@ const Contact: React.FC = () => {
           <h1 className={styles.contact__header__title}>Get In Touch</h1>
         </div>
         <div className={styles.contact__hero}>
-          <div className={styles.contact__hero__left}></div>
-          <div className={styles.contact__hero__right}></div>
+          <div className={styles.contact__hero__left}>
+            <h2 className={styles.contact__hero__subtitle}>Bring your projects to life</h2>
+          </div>
+          <div className={styles.contact__hero__right}>
+            <div className={styles.link__box}>
+              <div className={styles.link__box__header}>
+                <h3 className={styles.link__box__header__title}>Contact Info</h3>
+              </div>
+              <div className={styles.link__box__content}>
+                <ul>
+                  <li>
+                    <a href='mailto:enzo.mourany@outlook.com'>Email</a>
+                  </li>
+                  <li>
+                    <a href='https://www.linkedin.com/in/enzo-mourany/' target='_blank' rel='noreferrer'>LinkedIn</a>
+                  </li>
+                  <li>
+                    <a href='https://github.com/enzo-mourany' target='_blank' rel='noreferrer'>GitHub</a>
+                  </li>
+                  <li>
+                    <a href='#'>Calendly</a>
+                  </li>
+                  <li>
+                    <a href='#'>Project Request</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
         <div className={styles.contact__footer}>
           <div className={styles.contact__footer__left}>
+            <h3>Ressources</h3>
+            <ul>
+              <li><Link href='/'>Privacy Policy</Link></li>
+              <li><Link href='/'>Terms and Conditions</Link></li>
+              <li><Link href='/'>Blog</Link></li>
+            </ul>
           </div>
           <div className={styles.contact__footer__right}>
           </div>
