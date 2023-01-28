84c61522c6573f4f679d0012bcfd58517c3d0c2f
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-28 11:11:31 +0100

    feat: new structure

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -8,7 +8,22 @@ import styles from '../styles/Contact.module.scss';
 const Contact: React.FC = () => {
 
   return (
-    <div className={styles.contact}>
+    <div className={styles.contact__container}>
+      <div className={styles.contact__wrapper}>
+        <div className={styles.contact__header}>
+          <h1 className={styles.contact__header__title}>Get In Touch</h1>
+        </div>
+        <div className={styles.contact__hero}>
+          <div className={styles.contact__hero__left}></div>
+          <div className={styles.contact__hero__right}></div>
+        </div>
+        <div className={styles.contact__footer}>
+          <div className={styles.contact__footer__left}>
+          </div>
+          <div className={styles.contact__footer__right}>
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
