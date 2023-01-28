dcc4da26b8dcca2de81b1932eb602688742fe0e5
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-28 11:20:46 +0100

    fix: remove scrollingText

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,13 +3,11 @@ import React from 'react';
 import styles from '../../styles/ContactPage.module.scss';
 
 import Contact from '../../components/Contact';
-import ScrollingText from '../../components/ScrollingText';
 
 const Page: React.FC = () => {
   return (
     <div className={styles.contact__container}>
       <div className={styles.contact__wrapper}>
-        <ScrollingText text='contact' />
         <Contact />
       </div>
     </div>
