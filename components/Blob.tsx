6cd9da2dc7a6248ed6d44fad5393f65f62072b2e
Enzo Mourany <enzo.mourany@outlook.com>
2023-02-17 19:47:13 +0100

    fix: document bug

--- components/Blob.tsx
+++ components/Blob.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect } from 'react';
 
 import styles from '../styles/Blob.module.scss';
 
 export const Blob = () => {
+
+  useEffect(() => {
+    const blob = document.getElementById('blob');
+
+    const handlePointerMove = (event: { clientX: unknown; clientY: unknown; }) => {
+      const { clientX, clientY } = event;
+
+      if (blob) {
+        blob.style.left = `${clientX}px`;
+        blob.style.top = `${clientY}px`;
+      }
+    };
+
+    document.body.addEventListener('pointermove', handlePointerMove);
+
+    return () => {
+      document.body.removeEventListener('pointermove', handlePointerMove);
+    };
+  }, []);
+
   return (
-    <div className={styles.blob}></div>
+    <div id='blob' className={styles.blob}></div>
   );
 };
