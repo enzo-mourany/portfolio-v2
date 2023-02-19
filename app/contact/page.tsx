df7947194b98b211e9ab657b5b4f673a428079d3
Enzo Mourany <enzo.mourany@outlook.com>
2023-02-19 23:04:49 +0100

    fix: remove type

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,8 @@
-import type { NextPage } from 'next';
-
 import styles from '../../styles/ContactPage.module.scss';
 
 import Contact from '../../components/Contact';
 
-const ContactPage: NextPage = () => {
+const ContactPage = () => {
   return (
     <div className={styles.contact__container}>
       <div className={styles.contact__wrapper}>
