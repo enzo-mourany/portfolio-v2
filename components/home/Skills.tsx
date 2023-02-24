d6afa2b34877fdb0c425a559de0aee8f36959ee2
Enzo Mourany <enzo.mourany@outlook.com>
2023-02-24 10:58:32 +0100

    feat: add skills section

---
+++ components/home/Skills.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import styles from '../../styles/Skills.module.scss';
+
+export const Skills: React.FC = () => {
+  return (
+    <section>
+      <div id='skills' className={styles.container}>
+      </div>
+    </section>
+  )
+}
