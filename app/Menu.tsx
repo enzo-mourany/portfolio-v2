21c775d762d5fe6d716865ba515eb591f3de019c
Enzo Mourany <enzo.mourany@outlook.com>
2023-02-24 11:32:21 +0100

    feat: add Menu component

---
+++ app/Menu.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import LinkBox from '../components/LinkBox';
+
+interface MenuProps {
+  isMenuOpen: boolean;
+  setIsMenuOpen: (isMenuOpen: boolean) => void;
+}
+
+export const Menu: React.FC<MenuProps> = ({ isMenuOpen, setIsMenuOpen }) => {
+  const variants = {
+    open: { display: 'flex', opacity: 1 },
+    closed: { opacity: 0, transitionEnd: { display: 'none' } },
+  };
+
+  return (
+    <motion.div
+      className='hidden flex-col justify-center items-center fixed z-10 w-full h-full bg-black opacity-50'
+      variants={variants}
+      initial='closed'
+      animate={isMenuOpen ? 'open' : 'closed'}
+    >
+      <div>
+        <LinkBox isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      </div>
+    </motion.div>
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,22 @@
 'use client';
 
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
 
 import '../styles/globals.css';
-import LinkBox from '../components/LinkBox';
 import { AnalyticsWrapper } from '../components/analytics';
 import { Header } from './Header';
+import { Menu } from './Menu';
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-
-  const variants = {
-    open: { display: 'flex', opacity: 1 },
-    closed: { opacity: 0, transitionEnd: { display: 'none' } },
-  };
-
-  const menu: JSX.Element = (
-    <motion.div
-      className='hidden flex-col justify-center items-center fixed z-10 w-full h-full bg-black opacity-50'
-      variants={variants}
-      initial='closed'
-      animate={isMenuOpen ? 'open' : 'closed'}
-    >
-      <div>
-        <LinkBox isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-      </div>
-    </motion.div>
-  )
 
   return (
     <html>
       <head />
       <body className='bg-zinc-900'>
         <div>
-          {menu}
+          <Menu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
           <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
           {children}
           <AnalyticsWrapper />
