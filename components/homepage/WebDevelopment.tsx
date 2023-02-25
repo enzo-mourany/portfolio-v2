161546f7789d3e60269003f541263d8232e20741
Enzo Mourany <enzo.mourany@outlook.com>
2023-02-25 13:43:09 +0100

    feat: add marginBottom

--- components/homepage/WebDevelopment.tsx
+++ components/homepage/WebDevelopment.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BiCodeAlt } from 'react-icons/bi';
+import { MdWebAsset } from 'react-icons/md';
 
 import styles from '../../styles/WebDevelopment.module.scss';
 
@@ -11,16 +11,16 @@ export const WebDevelopment: React.FC = () => {
           <div className={styles.roadmap__line}>
             <div className={styles.icon}>
               <div className={styles.filter}></div>
-              <BiCodeAlt style={{ color: '#fff', width: 30, height: 30, zIndex: 1 }} />
+              <MdWebAsset style={{ color: '#fff', width: 30, height: 30, zIndex: 1 }} />
             </div>
             <div className={styles.line} />
           </div>
           <div id='description' className={styles.text}>
             <h3 className={styles.text__title}>Creating innovative and handcrafted websites</h3>
             <h2 className={styles.text__description}>
-              <span>I believe</span> that every website is unique and should be treated as such. That's why I take the time to understand each client's needs and create a custom design that reflects their business and vision. My handcrafted approach ensures that every website I create is unique and memorable.
+              <span>I believe</span> that every website is unique and should be treated as such.
             </h2>
-            <p>My development skills include using JavaScript, TypeScript, React, Next.js, Node.js, Express.js, MongoDB, PostgreSQL, HTML, CSS, Sass, Tailwind CSS and Java. I love using the latest technologies to create fast, user-friendly and responsive websites and mobile applications.</p>
+            <p>That's why I take the time to understand each client's needs and create a custom design that reflects their business and vision. My handcrafted approach ensures that every website I create is unique and memorable.</p>
           </div>
         </div>
       </div>
