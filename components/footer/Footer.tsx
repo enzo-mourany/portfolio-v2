d07204535bbfa7a9cbab0bedd977f128bdd72e15
Enzo Mourany <enzo.mourany@outlook.com>
2023-02-27 08:44:06 +0100

    feat: add footer component

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import React from 'react';
+
+import styles from '../../styles/Footer.module.scss';
+
+export const Footer: React.FC = () => (
+  <div id='footer' className={styles.container}>
+    <div className={styles.wrapper}>
+      <div className={styles.left}>
+        <ul>
+          <li>
+            <Link href='/contact'><p>Contact</p></Link>
+          </li>
+          <li>
+            <Link href='/contact'><p>Contact</p></Link>
+          </li>
+          <li>
+            <Link href='/contact'><p>Contact</p></Link>
+          </li>
+          <li>
+            <Link href='/contact'><p>Contact</p></Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.center}>
+        <p>Copyright @2023</p>
+      </div>
+      <div className={styles.right}>
+      </div>
+    </div>
+  </div>
+);
