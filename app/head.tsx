19d370d837dc58f3c5a8a98a5c0c10cc581dee69
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-27 18:36:04 +0200

    feat: add types

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable @next/next/next-script-for-ga */
 export default function Head() {
   return (
     <>
       <title>Enzo Mourany - Web Developer</title>
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <link rel='icon' href='/favicon.ico' />
+      <script async src='https://www.googletagmanager.com/gtag/js?id=G-BP5T37WVPL'>
+      </script>
     </>
   )
 }
