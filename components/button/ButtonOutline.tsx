8acc44c8d88ae9f664c19ffd2ee3e2077e657cdb
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-27 20:43:26 +0200

    feat: add ButtonOutline

---
+++ components/button/ButtonOutline.tsx
@@ -0,0 +1,3 @@
+export const ButtonOutline = () => (
+  <svg width='279' height='70' fill='none' xmlns='http://www.w3.org/2000/svg'><rect x='.5' y='.5' width='278' height='69' rx='34.5' stroke='#fff'/></svg>
+)
