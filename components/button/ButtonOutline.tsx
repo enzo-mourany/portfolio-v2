f23b7b9735cd259b0f43073f5b956c0c334ccf9a
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-27 21:45:59 +0200

    feat: add buttonOutline style

--- components/button/ButtonOutline.tsx
+++ components/button/ButtonOutline.tsx
@@ -1,27 +1,32 @@
+import styles from '../../styles/ButtonOutline.module.scss';
+
 export const ButtonOutline = () => (
-  <svg
-    width='279'
-    height='70'
-    fill='none'
-    xmlns='http://www.w3.org/2000/svg'>
-    <rect
-      x='.5'
-      y='.5'
-      width='278'
-      height='69'
-      rx='34.5'
-      stroke='#fff'
-      className='button-outline'
-    />
-    <rect
-      x='.5'
-      y='.5'
-      width='278'
-      height='69'
-      rx='34.5'
-      stroke='#fff'
-      className='filled'
-      pathLength={1}
-    />
-  </svg>
+  <button className={styles.button_outline}>
+    <svg
+      width='280'
+      height='70'
+      viewBox='-10 -10 300 90'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'>
+      <rect
+        x='.5'
+        y='.5'
+        width='278'
+        height='69'
+        rx='34.5'
+        stroke='#fff'
+        className={styles.bg_svg}
+      />
+      <rect
+        x='.5'
+        y='.5'
+        width='278'
+        height='69'
+        rx='34.5'
+        stroke='#fff'
+        className={styles.filled}
+        pathLength='1'
+      />
+    </svg>
+  </button>
 )
