e7a8bf67a6c149b39ee1f540885005def143421d
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-28 14:47:55 +0200

    finish Expertise section

--- components/homepage/Expertise.tsx
+++ components/homepage/Expertise.tsx
@@ -11,8 +11,15 @@ export const Expertise: React.FC = () => {
         <div className='p-20'>
           <h2 className='text-blue text-4xl'>Mon expertise en temps que développeur</h2>
         </div>
-        <div></div>
-        <div></div>
+        <div className='flex flex-row justify-center gap-5'>
+          <Card title='Front-end' description='Je développe des interfaces web responsives et performantes avec React, Next.js, Tailwind CSS, TypeScript, HTML et CSS.' />
+          <Card title='Back-end' description='Je développe des API REST avec Node.js, Express, MongoDB, Mongoose, TypeScript, HTML et CSS.' />
+          <Card title='Mobile' description='Je développe des applications mobiles avec React Native, Expo, TypeScript, HTML et CSS.' />
+          <Card title='CMS' description='Je développe des sites web avec WordPress, WooCommerce, Elementor, Divi, Beaver Builder, Gutenberg, HTML et CSS.' />
+        </div>
+        <div className='flex justify-center p-20'>
+          <ButtonOutline text='Améliorez votre visibilité' />
+        </div>
       </div>
     </section>
   )
