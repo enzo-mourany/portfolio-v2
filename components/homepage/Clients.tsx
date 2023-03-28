0efcd7daf714b27206056a82ecb9c99273f5632b
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-28 10:57:57 +0200

    feat: add responsive

--- components/homepage/Clients.tsx
+++ components/homepage/Clients.tsx
@@ -8,7 +8,7 @@ export const Clients: React.FC = () => {
           <h2 className='w-1/2 text-4xl'>Une image qui correspond à votre marque</h2>
         </div>
         <div className='flex w-1/2 h-full p-20'>
-          <p className='text-6xl text-blue leading-14'>Je sélectionne avec soin mes clients pour leur offrir un service d&apos;excellence, car traduire de grandes idées en grandes marques requiert une attention ciblée et une expertise de qualité.</p>
+          <p className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-blue leading-14'>Je sélectionne avec soin mes clients pour leur offrir un service d&apos;excellence, car traduire de grandes idées en grandes marques requiert une attention ciblée et une expertise de qualité.</p>
         </div>
       </div>
     </section>
