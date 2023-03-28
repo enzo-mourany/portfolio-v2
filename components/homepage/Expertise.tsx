778947bea8790da5cda36456f25f55aefea6cb77
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-28 16:34:30 +0200

    change content

--- components/homepage/Expertise.tsx
+++ components/homepage/Expertise.tsx
@@ -13,9 +13,9 @@ export const Expertise: React.FC = () => {
         </div>
         <div className='flex flex-row flex-wrap justify-center gap-5'>
           <Card title='Front-end' description='Je développe des interfaces web responsives et performantes avec React, Next.js, Tailwind CSS, TypeScript, HTML et CSS.' />
-          <Card title='Back-end' description='Je développe des API REST avec Node.js, Express, MongoDB, Mongoose, TypeScript, HTML et CSS.' />
-          <Card title='Mobile' description='Je développe des applications mobiles avec React Native, Expo, TypeScript, HTML et CSS.' />
-          <Card title='CMS' description='Je développe des sites web avec WordPress, WooCommerce, Elementor, Divi, Beaver Builder, Gutenberg, HTML et CSS.' />
+          <Card title='Back-end' description='Je développe des API REST avec Node.js, Express, MongoDB, Mongoose, TypeScript et PostgreSQL.' />
+          <Card title='Mobile' description='Je développe des applications mobiles avec React Native, TypeScript, et Flutter' />
+          <Card title='CMS' description='Je développe des sites web avec Webflow.' />
         </div>
         <div className='flex justify-center p-20'>
           <ButtonOutline text='Améliorez votre visibilité' />
