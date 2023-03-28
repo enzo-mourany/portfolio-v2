cf6a7a7f843adc4b577a7cc30c630bdd58e2def0
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-28 14:03:38 +0200

    feat: add two other cards

--- components/cards/ServiceCard.tsx
+++ components/cards/ServiceCard.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
+import { CgWebsite } from 'react-icons/cg'
 
 interface ServiceCardProps {
     title: string;
     description: string;
-    link: string;
 }
 
 export const ServiceCard: React.FC<ServiceCardProps> = ({ title, description }) => {
   return (
-    <div className='flex flex-col items-center justify-center gap-6 w-96 pt-12 pb-12 p-7 rounded-lg bg-gray-200'>
+    <div className='flex flex-col items-center justify-center gap-6 w-96 p-12 rounded-lg bg-gray-100 cursor-pointer'>
+      <div className='p-8'>
+        <CgWebsite className='text-6xl text-blue' />
+      </div>
       <h3 className='text-3xl'>
         {title}
       </h3>
-      <p className='w-3/4 text-lg text-center'>
+      <p className='w-4/5 text-lg text-center opacity-50'>
         {description}
-      </p>
-      <p className='mt-4'>
-        En savoir plus
       </p>
     </div>
   )
--- components/homepage/Services.tsx
+++ components/homepage/Services.tsx
@@ -10,8 +10,10 @@ export const Services: React.FC = () => {
           <h2 className='w-3/5 lg:w-3/4 text-w-xs lg:text-w-base text-blue text-center'>Services</h2>
           <h3 className='w-3/5 lg:w-3/4 text-3xl lg:text-w-4xl opacity-50 text-center'>Je construis votre marque pour que vous puissiez vous concentrer sur le reste.</h3>
         </div>
-        <div>
+        <div className='flex flex-row gap-16'>
           <ServiceCard title='Site Vitrine' description='Service de création de sites vitrines de qualité supérieure pour une présence en ligne professionnelle et convaincante.' />
+          <ServiceCard title='Landing Page' description='Service de création de landing pages efficaces pour une conversion maximale et un retour sur investissement optimal.' />
+          <ServiceCard title='Web App' description='Développement d’applications web personnalisées et performantes pour une expérience utilisateur exceptionnelle.' />
         </div>
       </div>
     </section>
