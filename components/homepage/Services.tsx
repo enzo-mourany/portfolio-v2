7359357d6c5fa85b48b26a3291c0a7191b506a56
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-28 12:34:44 +0200

    fix: text size

--- components/homepage/Services.tsx
+++ components/homepage/Services.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 export const Services: React.FC = () => {
   return (
-    <section id='clients' className='h-screen'>
+    <section id='services' className='h-screen'>
       <div className='flex flex-col lg:flex-row h-full'>
-        <div className='flex w-full lg:w-1/2 h-full p-20'>
-          <h2 className='w-3/5 lg:w-3/4 text-w-base lg:text-w-base text-blue'>Services</h2>
+        <div className='flex flex-col items-center w-full h-full p-20'>
+          <h2 className='w-3/5 lg:w-3/4 text-w-xs lg:text-w-base text-blue text-center'>Services</h2>
+          <h3 className='w-3/5 lg:w-3/4 text-3xl lg:text-w-4xl opacity-50 text-center'>Je construis votre marque pour que vous puissiez vous concentrer sur le reste.</h3>
         </div>
       </div>
     </section>
