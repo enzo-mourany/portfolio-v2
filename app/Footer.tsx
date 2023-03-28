fa6f8e659a59633a8dd1a74dfee142d2ade79182
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-28 15:11:05 +0200

    feat: add links

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 import { ButtonOutline } from '../components/button/ButtonOutline';
 
@@ -9,11 +10,38 @@ export const Footer: React.FC = () => (
         <h2 className='text-white'>Contactez-moi pour parlez de votre projet</h2>
         <ButtonOutline text='Discuttons' />
       </div>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className='flex flex-row gap-8'>
+        <ul className='flex flex-col text-white text-xl gap-2'>
+          <li className='font-medium'>Navigation</li>
+          <li className='opacity-50'>
+            <Link href='/'>Accueil</Link>
+          </li>
+          <li className='opacity-50'>Services</li>
+          <li className='opacity-50'>Blog</li>
+          <li className='opacity-50'>Contact</li>
+        </ul>
+        <ul className='flex flex-col text-white text-xl gap-2'>
+          <li className='font-medium'>Services</li>
+          <li className='opacity-50'>
+            <Link href='/'>Site Vitrine</Link>
+          </li>
+          <li className='opacity-50'>Landing Page</li>
+          <li className='opacity-50'>Web App</li>
+          <li className='opacity-50'>Mobile App</li>
+        </ul>
+        <ul className='flex flex-col text-white text-xl gap-2'>
+          <li className='font-medium'>Informations</li>
+          <li className='opacity-50'>Développeur Web et Mobile</li>
+          <li className='opacity-50'>Périgueux, France</li>
+        </ul>
+        <ul className='flex flex-col text-white text-xl gap-2'>
+          <li className='font-medium'>Contact</li>
+          <li className='opacity-50'>contact@enzomourany.com</li>
+          <li className='opacity-50'>LinkedIn</li>
+          <li className='opacity-50'>Malt</li>
+          <li className='opacity-50'>Calendly</li>
+          <li className='opacity-50'>GitHub</li>
+        </ul>
       </div>
     </div>
     <div></div>
