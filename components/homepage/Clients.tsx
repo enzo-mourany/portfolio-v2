b3b514346c789600ad4f58a0e4239644e34f1b1b
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-28 10:55:14 +0200

    feat: add new Clients section

--- components/homepage/Clients.tsx
+++ components/homepage/Clients.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export const Clients: React.FC = () => {
   return (
-    <section>
-      <div>
-        <div>
-          <h2>Une image qui correspond à votre marque</h2>
+    <section id='clients' className='h-screen'>
+      <div className='flex flex-row h-full'>
+        <div className='flex w-1/2 h-full p-20'>
+          <h2 className='w-1/2 text-4xl'>Une image qui correspond à votre marque</h2>
         </div>
-        <div>
-          <p>Je sélectionne avec soin mes clients pour leur offrir un service d&apos;excellence, car traduire de grandes idées en grandes marques requiert une attention ciblée et une expertise de qualité.</p>
+        <div className='flex w-1/2 h-full p-20'>
+          <p className='text-6xl text-blue leading-14'>Je sélectionne avec soin mes clients pour leur offrir un service d&apos;excellence, car traduire de grandes idées en grandes marques requiert une attention ciblée et une expertise de qualité.</p>
         </div>
       </div>
     </section>
