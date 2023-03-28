98033e363e6679cc44de97c4dc3a7453b91dd8ea
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-28 11:22:40 +0200

    feat: add responsive

--- components/homepage/Clients.tsx
+++ components/homepage/Clients.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 export const Clients: React.FC = () => {
   return (
     <section id='clients' className='h-screen'>
-      <div className='flex flex-row h-full'>
-        <div className='flex w-1/2 h-full p-20'>
-          <h2 className='w-1/2 text-4xl'>Une image qui correspond à votre marque</h2>
+      <div className='flex flex-col lg:flex-row h-full'>
+        <div className='flex w-full lg:w-1/2 h-full p-20'>
+          <h2 className='w-3/5 lg:w-3/4 text-w-base lg:text-w-xs'>Une image qui correspond à votre marque</h2>
         </div>
-        <div className='flex w-1/2 h-full p-20'>
-          <p className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-blue leading-14'>Je sélectionne avec soin mes clients pour leur offrir un service d&apos;excellence, car traduire de grandes idées en grandes marques requiert une attention ciblée et une expertise de qualité.</p>
+        <div className='flex w-full lg:w-1/2 h-full p-20'>
+          <p className='text-w-xl lg:text-w-base text-blue leading-tight'>Je sélectionne avec soin mes clients pour leur offrir un service d&apos;excellence, car traduire de grandes idées en grandes marques requiert une attention ciblée et une expertise de qualité.</p>
         </div>
       </div>
     </section>
