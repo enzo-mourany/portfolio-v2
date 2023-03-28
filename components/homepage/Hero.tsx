2d24dffe59a206abd189f956f43a23d722491c64
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-28 18:27:14 +0200

    change title and blob

--- components/homepage/Hero.tsx
+++ components/homepage/Hero.tsx
@@ -5,7 +5,7 @@ import styles from '../../styles/Hero.module.scss';
 const Hero: React.FC = () => (
   <section className={styles.container} id='hero__container'>
     <div className={styles.titles}>
-      <h1 className={styles.main__title} id='title'>Créer des solutions web pour donner de la visibilité à votre marque</h1>
+      <h1 className={styles.main__title} id='title'>Donner de la visibilité aux entreprises de demain</h1>
     </div>
     <div className={styles.description}>
       <p id='description'>
