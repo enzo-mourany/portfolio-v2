590844fc1387a79a44a8a55c512694e350f17790
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-29 10:56:17 +0200

    feat: add services pages

---
+++ app/services/mobileapp/page.tsx
@@ -0,0 +1,7 @@
+const MobileApp = () => (
+  <div>
+    <h1>Services</h1>
+  </div>
+);
+
+export default MobileApp;
---
+++ app/services/showcase/page.tsx
@@ -0,0 +1,7 @@
+const Showcase = () => (
+  <div>
+    <h1>Services</h1>
+  </div>
+);
+
+export default Showcase;
