4d8cf2bbfd231ee1ad62eaeb4312b023455fd867
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-29 10:32:46 +0200

    feat: add Services page

---
+++ app/services/page.tsx
@@ -0,0 +1,9 @@
+const Services = () => {
+  return (
+    <div className='flex flex-col items-center justify-center w-full h-full'>
+      <h1>Services</h1>
+    </div>
+  );
+};
+
+export default Services;
