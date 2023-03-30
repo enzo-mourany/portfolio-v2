5852d98d7a1cbea548013f1d7892632172fae209
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-30 14:32:15 +0200

    new ui

--- app/contact/project-request/page.tsx
+++ app/contact/project-request/page.tsx
@@ -106,65 +106,100 @@ const Page = () => {
                 </div>
               </div>
 
-              <div className='flex flex-col'>
-                <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
-                  <label htmlFor='name'>Position and Company</label>
-                  <div className='flex flex-row items-center justify-center gap-6'>
-                    <div className='mt-2'>
-                      <div className='flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md'>
-                        <input
-                          type='text'
-                          name='position'
-                          id='position'
-                          autoComplete='position'
-                          className='block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6'
-                          placeholder='CEO'
-                          value={position}
-                          onChange={(e) => setPosition(e.target.value)}
-                        />
-                      </div>
+              <div className='flex flex-col mt-10 gap-x-6 gap-y-8'>
+                <label htmlFor='name'>Position and Company</label>
+                <div className='flex flex-row items-center justify-center gap-6'>
+                  <div className='mt-2'>
+                    <div className='flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md'>
+                      <input
+                        type='text'
+                        name='position'
+                        id='position'
+                        autoComplete='position'
+                        className='block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6'
+                        placeholder='CEO'
+                        value={position}
+                        onChange={(e) => setPosition(e.target.value)}
+                      />
                     </div>
-                    <p>chez</p>
-                    <div className='mt-2'>
-                      <div className='flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md'>
-                        <input
-                          type='text'
-                          name='company'
-                          id='company'
-                          autoComplete='company'
-                          className='block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6'
-                          placeholder='Google'
-                          value={company}
-                          onChange={(e) => setCompany(e.target.value)}
-                        />
-                      </div>
+                  </div>
+                  <p>chez</p>
+                  <div className='mt-2'>
+                    <div className='flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md'>
+                      <input
+                        type='text'
+                        name='company'
+                        id='company'
+                        autoComplete='company'
+                        className='block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6'
+                        placeholder='Google'
+                        value={company}
+                        onChange={(e) => setCompany(e.target.value)}
+                      />
                     </div>
                   </div>
                 </div>
               </div>
-              <div className='flex flex-col'>
-                <label htmlFor='budget'>Budget</label>
-                <input
-                  type='text'
-                  id='budget'
-                  name='budget'
-                  placeholder='3000 - 5000€'
-                  value={budget}
-                  onChange={(e) => setBudget(e.target.value)}
-                />
+
+              <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
+                <div className='sm:col-span-4'>
+                  <label htmlFor='username' className='block text-sm font-medium leading-6 text-gray-900'>
+                    Budget
+                  </label>
+                  <div className='mt-2'>
+                    <div className='flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md'>
+                      <input
+                        type='text'
+                        name='budget'
+                        id='budget'
+                        autoComplete='budget'
+                        className='block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6'
+                        placeholder='2000'
+                        value={budget}
+                        onChange={(e) => setBudget(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className='flex flex-col'>
-                <label htmlFor='description'>Project Description</label>
-                <textarea
-                  name='description'
-                  id='description'
-                  cursor-ix='true'
-                  placeholder='Tell me more about your project. Please include details like goals, timeline, and design links if available.' value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                >
-                </textarea>
+
+              <div className='col-span-full'>
+                <label htmlFor='about' className='block text-sm font-medium leading-6 text-gray-900'>
+                  Message
+                </label>
+                <div className='mt-2'>
+                  <textarea
+                    id='about'
+                    name='about'
+                    rows={3}
+                    className='block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6'
+                    placeholder='Dites m&apos;en plus sur votre projet. Veuillez inclure des détails tels que les objectifs, le calendrier et les liens de conception, le cas échéant.'
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </div>
               </div>
-              <button type='submit'>Submit</button>
+
+              <div className='relative flex gap-x-3'>
+                <div className='flex h-6 items-center'>
+                  <input
+                    id='offers'
+                    name='offers'
+                    type='checkbox'
+                    className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600'
+                  />
+                </div>
+                <div className='text-sm leading-6'>
+                  <p className='text-gray-500'>Recevez des notifications de mes offres</p>
+                </div>
+              </div>
+
+              <button
+                type='submit'
+                className='rounded-md bg-blue hover:bg-white px-3 py-2 text-sm font-semibold text-white hover:text-blue hover:border-blue hover:border-solid hover:border-2 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-all duration-100 ease-in-out'
+              >
+                Save
+              </button>
             </form>
           </div>
         ) : (
