7b8684766c41cc7a9faf608b6074a4f3dea2fe36
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-31 23:52:04 +0200

    SEO optimization

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
   return (
     <>
       <title>Enzo Mourany - Création de sites web</title>
+      <meta name='description' content='Création de sites web pour les entreprises, startups et freelances | Développeur Web et Mobile basé sur Périgueux, Dordogne' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <link rel='icon' href='/favicon.ico' />
       <script async src='https://www.googletagmanager.com/gtag/js?id=G-BP5T37WVPL'>
