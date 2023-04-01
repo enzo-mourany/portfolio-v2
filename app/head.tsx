b069e27945edc61f8fe3aae873bb69ba1891bcec
Enzo Mourany <enzo.mourany@outlook.com>
2023-04-01 09:59:23 +0200

    feat: add GoogleAnalytics file

--- app/head.tsx
+++ app/head.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/next-script-for-ga */
 export default function Head() {
   return (
     <>
@@ -6,8 +5,6 @@ export default function Head() {
       <meta name='description' content='Création de sites web pour les entreprises, startups et freelances | Développeur Web et Mobile basé sur Périgueux, Dordogne' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <link rel='icon' href='/favicon.ico' />
-      <script async src='https://www.googletagmanager.com/gtag/js?id=G-BP5T37WVPL'>
-      </script>
     </>
   )
 }
