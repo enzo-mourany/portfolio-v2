e1dab85cb13754a3ddb85a10d1bfa17997406435
Enzo Mourany <enzo.mourany@outlook.com>
2023-04-03 21:01:16 +0200

    feat: add canonical tag

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,7 @@ export default function Head() {
       <meta name='description' content='Création de sites web pour les entreprises, startups et freelances | Développeur Web et Mobile basé sur Périgueux, Dordogne' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <link rel='icon' href='/favicon.ico' />
+      <link rel='canonical' href='https://enzomourany.com/' />
     </>
   )
 }
