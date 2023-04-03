f9dcbb392de14df355b157797a2d724e78fad9c8
Enzo Mourany <enzo.mourany@outlook.com>
2023-04-03 22:23:44 +0200

    feat: improve metadata

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,23 @@ export const metadata: Metadata = {
     template: '%s | Enzo Mourany',
   },
   description: 'Création de sites web pour les entreprises, startups et freelances | Développeur Web et Mobile basé sur Périgueux, Dordogne',
+  openGraph: {
+    type: 'website',
+    locale: 'fr_FR',
+    url: 'https://enzomourany.com/',
+  },
+  keywords: ['Développeur Web', 'Création de sites web', 'Site Web', 'Périgueux'],
+  authors: [{ name: 'Enzo Mourany', url: 'https://enzomourany.com' }],
+  colorScheme: 'light',
+  creator: 'Enzo Mourany',
+  referrer: 'origin-when-cross-origin',
+  applicationName: 'Enzo Mourany',
+  generator: 'Next.js',
+  viewport: 'initial-scale=1.0, width=device-width',
+  metadataBase: new URL('https://enzomourany.com/'),
+  alternates: {
+    canonical: '/',
+  },
 };
 
 export default function Page() {
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,11 @@
+import type { Metadata } from 'next';
+
 import Hero from './Hero';
 import Services from './Services';
+
+export const metadata: Metadata = {
+  title: 'Services',
+};
 
 const ServicesPage = () => {
   return (
