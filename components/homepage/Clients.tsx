9ebaa9d4181ca09877eff82195f7564d02cf6515
Enzo Mourany <enzo.mourany@outlook.com>
2023-04-06 20:16:05 +0200

    temp remove animation

--- components/homepage/Clients.tsx
+++ components/homepage/Clients.tsx
@@ -1,44 +1,11 @@
 'use client';
 
-import React, { useEffect, useRef } from 'react'
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import React from 'react'
 
 export const Clients: React.FC = () => {
-  const [lettersRef, setLettersRef] = useArrayRef();
-  const triggerRef = useRef<HTMLDivElement>(null);
-
-  function useArrayRef() {
-    const lettersRef = useRef<Array<React.RefObject<HTMLSpanElement>>>([]);
-    lettersRef.current = [];
-    return [lettersRef, (ref: React.RefObject<HTMLSpanElement>) => ref && lettersRef.current.push(ref)];
-  }
-
-  gsap.registerPlugin(ScrollTrigger);
 
   const text = 'Je sélectionne avec soin mes clients pour leur offrir un service d\'excellence, car traduire de grandes idées en grandes marques requiert une attention ciblée et une expertise de qualité.';
   const words = text.split(' ');
-
-  useEffect(() => {
-    const anim = gsap.to(
-      lettersRef.current!,
-      {
-        scrollTrigger: {
-          trigger: triggerRef.current as any,
-          scrub: true,
-          start: 'top center',
-          end: 'bottom 85%'
-        },
-        color: '#5160F0',
-        opacity: 1,
-        duration: 5,
-        stagger: 1,
-      }
-    );
-    return (() => {
-      anim.kill()
-    });
-  }, [lettersRef]);
 
   return (
     <section id='clients' className='w-screen h-screen relative mb-20 sm:mb-10 bg-white z-10'>
@@ -49,13 +16,11 @@ export const Clients: React.FC = () => {
         <div className='flex w-full lg:w-1/2 h-full p-8 sm:p-20'>
           <h4
             className='text-w-xl lg:text-w-base leading-tight'
-            ref={triggerRef}
           >
             {words.map((word, index) => (
               <span
                 key={index}
-                ref={setLettersRef}
-                className='inline-block text-blue opacity-20'
+                className='inline-block text-blue'
               >
                 {word}&nbsp;
               </span>
