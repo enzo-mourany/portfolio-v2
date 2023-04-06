d4e419589e809a919ac78095395785e3036457d7
Enzo Mourany <enzo.mourany@outlook.com>
2023-04-06 18:30:59 +0200

    feat: add text-reveal animation

--- components/homepage/Clients.tsx
+++ components/homepage/Clients.tsx
@@ -1,6 +1,45 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useRef } from 'react'
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 export const Clients: React.FC = () => {
+  const [lettersRef, setLettersRef] = useArrayRef();
+  const triggerRef = useRef<HTMLDivElement>(null);
+
+  function useArrayRef() {
+    const lettersRef = useRef<Array<React.RefObject<HTMLSpanElement>>>([]);
+    lettersRef.current = [];
+    return [lettersRef, (ref: React.RefObject<HTMLSpanElement>) => ref && lettersRef.current.push(ref)];
+  }
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const text = 'Je sélectionne avec soin mes clients pour leur offrir un service d\'excellence, car traduire de grandes idées en grandes marques requiert une attention ciblée et une expertise de qualité.';
+  const words = text.split(' ');
+
+  useEffect(() => {
+    const anim = gsap.to(
+      lettersRef.current!,
+      {
+        scrollTrigger: {
+          trigger: triggerRef.current as any,
+          scrub: true,
+          start: 'top center',
+          end: 'bottom 85%'
+        },
+        color: '#5160F0',
+        opacity: 1,
+        duration: 5,
+        stagger: 1,
+      }
+    );
+    return (() => {
+      anim.kill()
+    });
+  }, [lettersRef]);
+
   return (
     <section id='clients' className='w-screen h-screen relative mb-20 sm:mb-10 bg-white z-10'>
       <div className='flex flex-col lg:flex-row w-full'>
@@ -8,7 +47,20 @@ export const Clients: React.FC = () => {
           <h2 className='w-full sm:w-3/5 lg:w-3/4 text-xl sm:text-w-base lg:text-w-xs'>Une image qui correspond à votre marque</h2>
         </div>
         <div className='flex w-full lg:w-1/2 h-full p-8 sm:p-20'>
-          <h4 className='text-w-xl lg:text-w-base text-blue leading-tight'>Je sélectionne avec soin mes clients pour leur offrir un <strong>service d&apos;excellence</strong>, car traduire de grandes idées en grandes marques requiert une attention ciblée et une <strong>expertise</strong> de <strong>qualité</strong>.</h4>
+          <h4
+            className='text-w-xl lg:text-w-base leading-tight'
+            ref={triggerRef}
+          >
+            {words.map((word, index) => (
+              <span
+                key={index}
+                ref={setLettersRef}
+                className='inline-block text-blue opacity-20'
+              >
+                {word}&nbsp;
+              </span>
+            ))}
+          </h4>
         </div>
       </div>
     </section>
--- components/homepage/Hero.tsx
+++ components/homepage/Hero.tsx
@@ -1,80 +1,37 @@
 'use client';
 
 import React from 'react';
-import { motion } from 'framer-motion';
 
 import styles from '../../styles/Hero.module.scss';
 
 const Hero: React.FC = () => {
-  const title = 'Création de sites web pour les entreprises, startups et freelances';
-  const words: string[] = title.split(' ');
-
-  const sentenceVariants = {
-    hidden: { opacity: 0 },
-    visible: (i = 1) => ({
-      opacity: 1,
-      transition: { staggerChildren: 0.12, delayChildren: 0.5 + i * 0.04 },
-    })
-  };
-
-  const wordsVariants = {
-    hidden: { opacity: 0, y: 40 },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        type: 'spring',
-        damping: 16,
-        stiffness: 200
-      }
-    }
-  };
-
-  const descriptionVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 0.5,
-      transition: {
-        type: 'spring',
-        damping: 16,
-        stiffness: 200,
-        delay: 1.5
-      }
-    }
-  };
 
   return (
     <section className={styles.container} id='hero__container'>
       <div className={styles.titles}>
-        <motion.h1
+        <h1
           className={styles.main__title}
-          id='title'
-          variants={sentenceVariants}
-          initial='hidden'
-          animate='visible'
         >
-          {words.map((word: string) => (
-            <motion.span
-              key={word}
-              className={styles.title}
-              variants={wordsVariants}
-              initial='hidden'
-              animate='visible'
-            >
-              {word}&nbsp;
-            </motion.span>
-          ))}
-        </motion.h1>
+          <span className={styles.title} id='title'>
+            <span>Création de sites</span>
+          </span>
+          <span className={styles.title} id='title'>
+            <span>web pour les </span>
+          </span>
+          <span className={styles.title} id='title'>
+            <span>entreprises, startups </span>
+          </span>
+          <span className={styles.title} id='title'>
+            <span>et freelances </span>
+          </span>
+        </h1>
       </div>
       <div className={styles.description}>
-        <motion.h2
+        <h2
           id='description'
-          variants={descriptionVariants}
-          initial='hidden'
-          animate='visible'
         >
           Développeur web et mobile basé en France à Périgueux, spécialisé dans la création de solutions web pour les freelances, startups et entreprises
-        </motion.h2>
+        </h2>
       </div>
     </section>
   );
