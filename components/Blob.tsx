32ae5c1b468d70d36f76f5b1a05d0451650e7e03
Enzo Mourany <enzo.mourany@outlook.com>
2023-04-10 16:19:55 +0200

    improve blob

--- components/Blob.tsx
+++ components/Blob.tsx
@@ -2,9 +2,6 @@ import styles from '../styles/Blob.module.scss';
 
 export const Blob = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.blob}></div>
-      <div className={styles.blur}></div>
-    </div>
+    <div className={styles.gradient}></div>
   )
 };
